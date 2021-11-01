793416189fd509e329a1e2e1dbc48ff389612f79
TheGewehr <alexandre07979@gmail.com>
2021-11-01 12:46:19 +0100

    Tying 2 loops implementation

--- Gauss_Elimination/Gauss.cpp
+++ Gauss_Elimination/Gauss.cpp
@@ -20,7 +20,7 @@ int RandomRange(int value01, int value02) {
 
 }
 
-void ForwardThreeLoop(int matSize, bool autoFill)
+void ForwardThreeLoops(int matSize, bool autoFill)
 {
 	//int matSize = 0;
 	int i = 0;
@@ -231,6 +231,215 @@ void ThreeLoops_5x5()
 
 }
 
+void ForwardTwoLoops(const int matSize, bool autoFill)
+{
+	//int matSize = 0;
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+
+
+	// Augmented Matrix = Matrix + results
+	//initialize matrix of n size
+	float* x = new float[matSize * sizeof(float)];
+	for (int a = 0; a < matSize; a++)
+	{
+		x[a] = 0;
+	}
+
+	float** Mat = new float* [matSize * sizeof(float)];
+
+	for (int a = 0; a < matSize; a++)
+	{
+		*(Mat + a) = new float[(matSize + 1) * sizeof(float)];
+	}
+
+	int range = 0;
+	if (autoFill)
+	{
+
+		cout << "\tEnter the range of numbers [-n to n] to fill the matrix with:" << endl;
+		cin >> range;
+		cout << endl;
+
+	}
+
+	bool ret = false;
+
+	while (!ret)
+	{
+
+		for (int a = 0; a < matSize; a++)
+		{
+			for (int b = 0; b < matSize + 1; b++)
+			{
+				Mat[a][b] = 0;
+			}
+
+		}
+		//End mat init
+
+
+
+		if (!autoFill)
+		{
+			//User inputs the matrix elements by rows
+			cout << "Enter Elements of " << matSize << " Rows & " << matSize + 1 << " Columns\n";
+			cout << fixed;
+
+			for (i = 0; i < matSize; i++)
+			{
+				cout << "\tEnter Row  " << i + 1 << " & Press Enter\n";
+				for (j = 0; j < matSize + 1; j++)
+					cin >> Mat[i][j];
+			}
+
+		}
+		else
+		{
+
+			cout << endl;
+			//inputs are automatic
+			for (i = 0; i < matSize; i++)
+			{
+				for (j = 0; j < matSize + 1; j++)
+					Mat[i][j] = RandomRange(-range, range);
+			}
+		}
+
+		cout << "\n Matrix is:\n\n";
+
+		for (i = 0; i < matSize; i++)
+		{
+			for (j = 0; j < matSize + 1; j++)
+				cout << setw(8) << setprecision(4) << Mat[i][j];
+			cout << endl;
+		}
+
+		//find values of x,y,z using back substitution
+
+
+
+		//Upper triangulation loops
+
+		int pivot = 0;
+		int subpivot = 0;
+
+		i = 0;
+		j = 0;
+		k = 0;
+
+		// 1 LOOP TRIANGULATION TRYAL
+
+		//for (int i = 1; i < matSize - 1; i++)
+		//		{
+		//			Mat[i + 1] [i] = Mat[i + 1] [i] / Mat[i] [i];
+		//			Mat[i + 1] [i + 1 ] = Mat[i + 1] [i + 1 ] - Mat[i + 1] [i] * Mat[i] [i + 1];
+		//		}
+
+
+		float li = 0;
+		int taco = matSize + 1;
+		//float b[taco];
+
+		for (int i = 0; i < matSize; i++)
+		{
+			//b[i] = Mat[i][matSize];
+		}
+
+		// A = Mat
+		// b = Vector de resultados, ultima columna de la matriz n*n+1
+
+		for (int k = 1; k < matSize ; k++)
+		{
+
+			for (int i = k + 1; i < matSize; i++)
+			{
+				li = Mat[i][k] / Mat[k][k];
+
+				Mat[i][matSize] = Mat[i][matSize] - li * Mat[k][matSize];
+				//b[i] = b[i] - li * b[k];
+			}
+
+		}
+
+		//ORIGINAL NOT COPY -> not the code we need.
+		//for (j = 0; j < N - 1; j++)
+		//{
+		//	for (i = j + 1; i < N; i++)
+		//	{
+		//		temp = Matrix[i][j] / Matrix[j][j];
+		//
+		//		for (k = 0; k < N + 1; k++)
+		//			Matrix[i][k] -= Matrix[j][k] * temp;
+		//	}
+		//}
+
+
+		//print the Upper Triangular matrix
+
+		cout << "\n ---------------------------------\n";
+		cout << "\n Upper Triangular Mat is:\n";
+		for (i = 0; i < matSize; i++)
+		{
+			for (j = 0; j < matSize + 1; j++)
+				cout << setw(8) << setprecision(4) << Mat[i][j];
+			cout << endl;
+		}
+
+		//find values of x,y,z using back substitution
+
+		cout << "\n ---------------------------------\n";
+
+		//right = accumulation of Matrix[i][j] * x[j]; (delivery 1 page 10)
+		float* right = new float[matSize * sizeof(float)];
+
+		for (int a = 0; a < matSize; a++)
+		{
+			right[a] = 0;
+		}
+
+
+		for (i = matSize - 1; i >= 0; i--)
+		{
+			for (j = i + 1; j < matSize; j++)
+			{
+				right[i] += Mat[i][j] * x[j];
+			}
+
+			if (Mat[i][i] != 0)
+			{
+
+				x[i] = (Mat[i][matSize] - right[i]) / Mat[i][i];
+				ret = true;
+			}
+			else
+			{
+				//matrix no valid for processing
+				cout << "Error: Trying to divide by 0." << endl;
+				ret = false;
+				break;
+			}
+
+
+		}
+	}
+
+
+	//print values of x,y,z
+
+	cout << "\n The Solution is:\n";
+	for (i = 0; i < matSize; i++)
+		cout << "x[" << setw(3) << i + 1 << "]=" << setw(7) << setprecision(4) << x[i] << endl;
+
+	//delete heap data
+	for (size_t a = 0; a < matSize; a++)
+	{
+		delete[] Mat[a];
+	}
+}
+
 int main()
 {
 	srand(time(NULL));
@@ -245,7 +454,9 @@ int main()
 	cin >> autoFill;
 	cout << endl;
 
-	ForwardThreeLoop(matSize, autoFill);
+	ForwardThreeLoops(matSize, autoFill);
+
+	//ForwardTwoLoops(matSize, autoFill);
 
 	
 
