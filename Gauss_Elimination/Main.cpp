cf03b3e21eb91d260477794de519663334734bec
IconicGIT <72080813+IconicGIT@users.noreply.github.com>
2021-11-24 23:38:50 +0100

    updated partial pivot elimination

--- Gauss_Elimination/Main.cpp
+++ Gauss_Elimination/Main.cpp
@@ -27,22 +27,23 @@ void ForwardThreeLoops(int matSize, bool autoFill)
 	int i = 0;
 	int j = 0;
 	int k = 0;
+	double temp = 0;
 
 	
 
 	// Augmented Matrix = Matrix + results
 	//initialize matrix of n size
-	long* x = new long[matSize * sizeof(long)];
+	double* x = new double[matSize * sizeof(double)];
 	for (int a = 0; a < matSize; a++)
 	{
 		x[a] = 0;
 	}
 		
-	long** Mat = new long* [matSize * sizeof(long)];
+	double** Mat = new double* [matSize * sizeof(double)];
 
 	for (int a = 0; a < matSize; a++)
 	{
-		*(Mat + a) = new long[(matSize + 1) * sizeof(long)];
+		*(Mat + a) = new double[(matSize + 1) * sizeof(double)];
 	}
 
 	float range = 0;
@@ -100,13 +101,14 @@ void ForwardThreeLoops(int matSize, bool autoFill)
 					
 			}
 		}
+		cout << endl;
 
 		cout << "\n Matrix is:\n\n";
 
 		for (i = 0; i < matSize; i++)
 		{
 			for (j = 0; j < matSize + 1; j++)
-				cout << setw(3) << setprecision(4) << Mat[i][j];
+				cout << setw(10) << setprecision(4) << Mat[i][j];
 			cout << endl;
 		}
 
@@ -119,46 +121,129 @@ void ForwardThreeLoops(int matSize, bool autoFill)
 		j = 0;
 		k = 0;
 
-		//start time stamp
-		auto Init = Clock::now();
+		
+		//Swap row with highest first element with the first row
 
+		double* tempLine = new double[sizeof(double) * (matSize + 1)];
+		bool tempFull = false;
 
-
-		//i counts the COLUMNS that must have zeros
-		for (i = 0; i < matSize - 1; i++)
+		for (size_t i = 0; i < matSize + 1; i++)
 		{
-			pivot = Mat[i][i];
+			tempLine[i] = 0;
+		}
+		int maxPos = 0;
 
-			//j counts the ROWS that must have zeros
-			for (j = i + 1; j < matSize; j++)
+		for (i = 1; i < matSize; ++i)
+		{
+			// Change < to > if you want to find the smallest element
+			if (Mat[0][0] < Mat[i][0])
 			{
-				subpivot = Mat[j][i];
-
-				//k counts the whole ROW that needs to be multiplied
-				for (k = i; k < matSize + 1; k++)
+				for (int a = 0; a < matSize + i; a++)
 				{
-					Mat[j][k] = pivot * Mat[j][k] - subpivot * Mat[i][k];
+					tempFull = true;
+					tempLine[a] = Mat[0][a];
+					maxPos = i;
 				}
 			}
 		}
+
+		if (tempFull)
+			for (int a = 0; a < matSize + i; a++)
+			{
+				Mat[0][a] = Mat[maxPos][a];
+				Mat[maxPos][a] = tempLine[a];
+			}
+
+		
+		
+		cout << endl;
+		cout << "Swap row with highest first element with the first row:" << endl;
+		cout << endl;
+
+		for (i = 0; i < matSize; i++)
+		{
+			for (j = 0; j < matSize + 1; j++)
+				cout << setw(10) << setprecision(4) << Mat[i][j];
+			cout << endl;
+		}
+
+		float warningPivot = 0.000000001f;
+		double pivotTemp = 0;
+		cout << endl;
+
+		cout << "		Start triangulation:" << endl;
+		cout << endl;
+
+		//start time stamp
+		auto Init = Clock::now();
+		for (j = 0; j < matSize - 1; j++)
+		{
+			pivot = (double)Mat[j][j];
+			for (i = j + 1; i < matSize; i++)
+			{
+				if (Mat[j][j] == 0)
+				{
+					cout << "Error: Trying to divide by 0. Matrix is incompatible or indeterminated." << endl;
+					ret = false;
+					break;
+				}
+				
+				pivotTemp = (double)Mat[i][j] / (double)Mat[j][j];
+				
+
+				cout << "Matrix current state: " << endl;
+				for (int a = 0; a < matSize; a++)
+				{
+					for (int b = 0; b < matSize + 1; b++)
+						cout << setw(10) << setprecision(4) << Mat[a][b];
+					cout << endl;
+				}
+				cout << "---------------------" << endl;
+				cout << "Matrix element [" << Mat[i][j] << "] at " << i << ", " << j << "." << endl;
+				cout << "Current pivot: [" << Mat[j][j] << "] at " << j << ", " << j << "." << endl;
+				cout << "Result of element / pivot: " << pivotTemp << endl;
+				cout << "Result of element - previous result * pivot: " << Mat[i][j] - Mat[j][j] * pivotTemp << endl;
+				cout << "---------------------" << endl;
+
+				cout << endl;
+
+				if (warningPivot > abs(pivotTemp) && pivotTemp != 0)
+				{
+					cout << endl;
+					cout << "pivot division dangerously low: [" << pivotTemp <<"]. Results may be affected." << endl;
+					cout << endl;
+				}
+				
+				cout << "		Next Iteration: " << endl;
+				cout << endl;
+				for (k = 0; k < matSize + 1; k++)
+					Mat[i][k] -= Mat[j][k] * pivotTemp;
+			}
+		}
+
+
+		////i counts the COLUMNS that must have zeros
+		//for (i = 0; i < matSize - 1; i++)
+		//{
+		//	pivot = Mat[i][i];
+		//
+		//	//j counts the ROWS that must have zeros
+		//	for (j = i + 1; j < matSize; j++)
+		//	{
+		//		subpivot = Mat[j][i];
+		//
+		//		//k counts the whole ROW that needs to be multiplied
+		//		for (k = i; k < matSize + 1; k++)
+		//		{
+		//			Mat[j][k] = pivot * Mat[j][k] - subpivot * Mat[i][k];
+		//		}
+		//	}
+		//}
 		
 		//end time stamp
 		auto End = Clock::now();
 		
-		//PARTIAL PIVOTING
-		/*
-				//for (j = 0; j < N - 1; j++)
-		//{
-		//	for (i = j + 1; i < N; i++)
-		//	{
-		//		temp = Matrix[i][j] / Matrix[j][j];
-		//
-		//		for (k = 0; k < N + 1; k++)
-		//			Matrix[i][k] -= Matrix[j][k] * temp;
-		//	}
-		//}
-		
-		*/
+	
 
 		//total triangulation time
 		executionTime = std::chrono::duration_cast<std::chrono::nanoseconds>(End - Init).count();
@@ -176,8 +261,8 @@ void ForwardThreeLoops(int matSize, bool autoFill)
 
 		//find values of x,y,z using back substitution
 
-		cout << "\n ---------------------------------\n";
-		cout << "\n ---------------------------------\n";
+		cout << "\n =================================\n";
+
 
 		//right = accumulation of Matrix[i][j] * x[j]; (delivery 1 page 10)
 		float* right = new float[matSize * sizeof(float)];
@@ -209,9 +294,34 @@ void ForwardThreeLoops(int matSize, bool autoFill)
 				delete right;
 				break;
 			}
+		}
 
+		bool indeterminated;
+		for (size_t a = 0; a < matSize; a++)
+		{
+			indeterminated = true;
+			for (size_t b = 0; b < matSize + 1; b++)
+			{
+				double c = abs(Mat[a][b]);
+				float d = 0.000001f;
+
+				if (c != 0 && c > d)
+				{
+					indeterminated = false;
+				}
+
+
+			}
+			if (indeterminated) break;
 
 		}
+
+		if (indeterminated)
+		{
+			cout << "The system is indeterminated!, result may not be correct." << endl;
+			cout << endl;
+		}
+
 	}
 
 
